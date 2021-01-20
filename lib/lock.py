702e70481d000851467bfb0bddf668aaf1567971
Kirill K <kovalev.kirill.a@gmail.com>
2021-01-21 01:19:23 +0300

    fix: remove dynamic fields as they wont work as expected

--- lib/lock.py
+++ lib/lock.py
@@ -1,8 +1,6 @@
 import time
 import uuid
 
-import datetime
-
 from lib.storage_object import StorageObject
 
 
@@ -14,7 +12,7 @@ class Lock(StorageObject):
     # Some random UUID to make lock UUIDs persistent
     ROOT_UUID = uuid.UUID("0bbbf540-1e6d-47f9-ab52-0d3d6c814018")
 
-    def __init__(self, storage, id, namespace, **kwargs):
+    def __init__(self, storage, namespace, id, **kwargs):
         self.id = id
         self.uuid = str(
             uuid.uuid5(
@@ -23,27 +21,17 @@ class Lock(StorageObject):
             )
         )
 
+        self.namespace = namespace
+
         super(Lock, self).__init__(storage=storage, id=self.uuid)
 
         self.ttl = kwargs.get("ttl", 60)
         self.timestamp = kwargs.get("timestamp", int(time.time()))
 
-        self.expires_timestamp = self._expires_timestamp
-        self.expires_human = self._expires_human
-        self.seconds_left = self._seconds_left
-
-    @property
-    def _expires_timestamp(self):
-        return self.timestamp + self.ttl
-
-    @property
-    def _expires_human(self):
-        return datetime.datetime.fromtimestamp(self.timestamp + self.ttl).strftime("%c")
-
-    @property
-    def _seconds_left(self):
-        return int(time.time()) - self.timestamp + self.ttl
-
     def _write(self):
+        self.namespace._load_self()
+        self.namespace.locks.append(self.id)
+        self.namespace.update()
+
         self._storage.create(self._storage_id, self._dump())
         self._storage.ttl(self._storage_id, self.ttl)
